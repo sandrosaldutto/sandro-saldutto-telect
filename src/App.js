3a940363be86e77c7a02638f4e6aff8ba4bb8f60
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-26 16:30:19 -0400

    adjusted backgound on app.scss and set up route for home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.scss';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from './components/Nav/Nav'
+import HomePage from "../src/pages/HomePage/HomePage"
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Nav/>
+    <Switch>
+      <Route
+      exact
+      path="/"
+      component={HomePage}
+     />
+    </Switch>
+    </BrowserRouter>
     </>
   );
 }
