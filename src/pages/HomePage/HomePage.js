69e21aba23f57fa71f867e2e0d423104f680b0ae
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-26 21:15:47 -0400

    added new realse card for an individual show or episode of a show

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import "./HomePage.scss";
+import NewRelease from "../../components/NewRelease/NewRelease"
 
 function HomePage() {
   return (
     <section className="home">
-      
+      <h1 className="home__title">New Releases</h1>
+      <NewRelease/>        
     </section>
   );
 }
