5f849f2383b1977e6526180de3b7936ccf292a96
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-31 15:51:42 -0400

    in process of adding show to my list

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,16 +1,22 @@
 import "./Switch.scss";
 import { useState } from "react";
+import axios from 'axios';
 
-function Switch({ id }) {
-  const [toggle, setToggle] = useState(false);
+function Switch({ showId }) {
+  const [toggle, setToggle] = useState(true);
   
   const toggler = () => {
     toggle ? setToggle(false) : setToggle(true);
     console.log(toggle)
   };
 
-  const handleToggle = (e, id) => {
-    console.log(id);
+  const handleToggle = (e, showId) => {
+    console.log(showId);
+    axios
+      .post("http://localhost:8080/mylist/", {
+        showId: showId,
+        userId: 1
+      })
   };
 
   return (
@@ -19,7 +25,7 @@ function Switch({ id }) {
         <input
           onClick={toggler}
           onChange={(e) => {
-            handleToggle(e, id);
+            handleToggle(e, showId);
           }}
           type="checkbox"
           className="switch__input"
