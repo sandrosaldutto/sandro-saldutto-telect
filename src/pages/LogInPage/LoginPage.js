c4c3d46b23e6173016814f8ed0ccf509059a6c5b
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-31 09:50:23 -0400

    added some styling to the login and sign up page

--- src/pages/LogInPage/LoginPage.js
+++ src/pages/LogInPage/LoginPage.js
@@ -1,44 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import FormInputs from '../../components/FormInputs/FormInputs';
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import FormInputs from "../../components/FormInputs/FormInputs";
 import "./LoginPage.scss";
 
 function Login(props) {
-    const userLogin = (e) => {
-        e.preventDefault();
+  const userLogin = (e) => {
+    e.preventDefault();
 
-        console.log({
-            username: e.target.username.value,
-            password: e.target.password.value,
-        })
+    console.log({
+      username: e.target.username.value,
+      password: e.target.password.value,
+    });
 
-        axios.post('http://localhost:8080/users/login', {
-            username: e.target.username.value,
-            password: e.target.password.value,
-        })
-        .then(res => {
-            console.log(res)
-            let token = res.data.token;
-            sessionStorage.setItem('authToken', token)
-            props.history.push('/')
-        })
-    }
+    axios
+      .post("http://localhost:8080/users/login", {
+        username: e.target.username.value,
+        password: e.target.password.value,
+      })
+      .then((res) => {
+        console.log(res);
+        let token = res.data.token;
+        sessionStorage.setItem("authToken", token);
+        props.history.push("/");
+      });
+  };
 
-    return (
-        <div>
-            <h1>
-                Log In
-            </h1>
-            <form onSubmit={userLogin}>
-                <FormInputs label="Username" name="username" type="text" />
-                <FormInputs label="Password" name="password" type="password" />
-
-                <button type="submit">Log In</button>
-            </form>
-            <Link to="/signup">Sign Up</Link>
-        </div>
-    )
+  return (
+    <div className="login">
+      <div className="login__title-container">
+        <h1 className="login__title">Log In</h1>
+      </div>
+      <form className="login__form" onSubmit={userLogin}>
+        <FormInputs placeholder="Type username here" label="Username" name="username" type="text" />
+        <FormInputs placeholder="Type password here" label="Password" name="password" type="password" />
+        <button className="login__login-button" type="submit">
+          Log In
+        </button>
+      </form>
+      <Link to="/signup">
+          <button  className="login__signup-button">Sign Up</button>
+      </Link>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,43 +1,61 @@
-import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import FormInputs from '../../components/FormInputs/FormInputs';
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import FormInputs from "../../components/FormInputs/FormInputs";
 import "./SignUpPage.scss";
 
 function SignUp(props) {
-    const userSignUp = (e) => {
-        e.preventDefault();
+  const userSignUp = (e) => {
+    e.preventDefault();
 
-        console.log({
-            name: e.target.name.value,
-            username: e.target.username.value,
-            password: e.target.password.value,
-        })
+    console.log({
+      name: e.target.name.value,
+      username: e.target.username.value,
+      password: e.target.password.value,
+    });
 
-        axios.post('http://localhost:8080/users/register', {
-            name: e.target.name.value,
-            username: e.target.username.value,
-            password: e.target.password.value,
-        })
-        .then(res => {
-            console.log(res)
-            props.history.push('/login')
-        })
-    }
+    axios
+      .post("http://localhost:8080/users/register", {
+        name: e.target.name.value,
+        username: e.target.username.value,
+        password: e.target.password.value,
+      })
+      .then((res) => {
+        console.log(res);
+        props.history.push("/login");
+      });
+  };
 
-    return (
-        <div className="signup">
-            <h1>
-                Sign Up
-            </h1>
-            <form onSubmit={userSignUp}>
-                <FormInputs placeholder="Type name here" label="Name" name="name" type="text" />
-                <FormInputs placeholder="Type username here" label="Username" name="username" type="text" />
-                <FormInputs placeholder="Type password here" label="Password" name="password" type="password" />
-                <button type="submit">Sign Up!</button>
-            </form>
-            <Link to="/login">Log In</Link>
-        </div>
-    )
+  return (
+    <div className="signup">
+      <div className="signup__title-container">
+        <h1 className="signup__title">Log In</h1>
+      </div>
+      <form className="signup__form" onSubmit={userSignUp}>
+        <FormInputs
+          placeholder="Type name here"
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <FormInputs
+          placeholder="Type username here"
+          label="Username"
+          name="username"
+          type="text"
+        />
+        <FormInputs
+          placeholder="Type password here"
+          label="Password"
+          name="password"
+          type="password"
+        />
+        <button className="signup__signup-button" type="submit">Sign Up!</button>
+      </form>
+      <Link to="/login">
+          <button  className="signup__login-button">Log In</button>
+      </Link>
+    </div>
+  );
 }
-export default SignUp
+export default SignUp;
