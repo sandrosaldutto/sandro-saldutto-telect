5b86f1e050bab9f8691dd3587ebb0d800c0df214
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-31 10:27:45 -0400

    added some animations to my nave and fixed some styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,23 @@
 import "./Nav.scss";
 import logoIcon from "../../assets/logos/logo-icon.svg";
 import listIcon from "../../assets/icons/list.svg";
-import profileIcon from "../../assets/icons/profile.svg";
+import loginSignupIcon from "../../assets/icons/login-signup.svg";
 import wordmarkIcon from "../../assets/logos/logo-wordmark.svg"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function Nav() {
   return (
     <section className="nav">
-      <img src={profileIcon} alt="" className="nav__profile"></img>
-      <img src={logoIcon} alt="" className="nav__logo"></img>
-      <img src={wordmarkIcon} alt="" className="nav__wordmark"></img>
-      <img src={listIcon} alt="" className="nav__list"></img>
+      <Link to="/signup">
+      <img src={loginSignupIcon} alt="Login-signup" className="nav__login-signup"/>
+      </Link>
+      <Link to="/">
+      <img src={logoIcon} alt="Home" className="nav__logo"/>
+      </Link>
+      {/* <Link to="/"> */}
+      <img src={wordmarkIcon} alt="Home" className="nav__wordmark"></img>
+      {/* </Link> */}
+      <img src={listIcon} alt="My list" className="nav__list"></img>
     </section>
   );
 }
