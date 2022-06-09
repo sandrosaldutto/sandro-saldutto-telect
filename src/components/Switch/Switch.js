60cf082a6c2827a571d8cf04626492021d4cb514
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-06-09 12:13:09 -0400

    trying to fix cors issue

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,7 +1,7 @@
 import "./Switch.scss";
 import { useState } from "react";
 import axios from "axios";
-import { API_URL } from "../../config/index"
+import { API_URL } from "../../config/index";
 
 function Switch({ showId }) {
   const [toggle, setToggle] = useState(true);
@@ -26,25 +26,27 @@ function Switch({ showId }) {
 
   return (
     <section>
-      { showLogout ? (  <label className="switch">
-        <input
-          onClick={toggler}
-          onChange={() => {
-            handleToggle(showId);
-          }}
-          type="checkbox"
-          className="switch__input"
-        />
-        <span className="switch__slider" />
-      </label>) : ( <label className="switch">
-        <input
-          type="checkbox"
-          className="switch__input--disable"
-        />
-        <span className="switch__slider--disable" />
-      </label>)}
-  
-      </section>
+      {showLogout ? (
+        <label className="switch">
+          <input
+            onClick={toggler}
+            onChange={() => {
+              handleToggle(showId);
+            }}
+            type="checkbox"
+            className="switch__input"
+          />
+          <span className="switch__slider" />
+        </label>
+      ) : (
+        <label
+          className="switch"
+        >
+          <input type="checkbox" className="switch__input--disable" />
+          <span className="switch__slider--disable" />
+        </label>
+      )}
+    </section>
   );
 }
 
