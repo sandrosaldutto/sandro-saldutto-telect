8706f1ed1586dcd981d5cec1f6907106d821916b
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-12-07 18:03:40 -0700

    finished

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,4 @@
-// export const API_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://telect-server.herokuapp.com"
-//     : "http://localhost:8080";
-
-export const API_URL = "https://telect-server.herokuapp.com";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://telect-server.herokuapp.com"
+    : "http://localhost:8080";
